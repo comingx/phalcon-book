962989267f2fcc5a78625fae89ea59e24b4602b8
ComingX <comingx@sina.com>
2019-08-31 10:49:09 +0800

    Create README.mk

---
+++ README.mk
@@ -0,0 +1,10 @@
+Phalcon深度实践纠正
+#P158
+1.
+>class IndexController extends Controller
+改为
+class Module implements ModuleDefinitionInterface
+
+2.
+>public function registerServices(DiInterface $di)
+后移四个英文空格
